1e88e8dc3aac10d1fca6054612cb7251be1fa2f1
Tanisha Banik <26tanishabanik@gmail.com>
2022-06-21 23:44:48 +0530

    removed unecessary comments from the code

--- generateGraph.go
+++ generateGraph.go
@@ -2,22 +2,16 @@ package graph
 
 import (
 	"context"
-	// "encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"time"
-
-	//"math/rand"
 	"github.com/wcharczuk/go-chart/v2"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	// corev1 "k8s.io/api/core/v1"
-	// histogram "kubernetes-go/histogram"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	metrics "k8s.io/metrics/pkg/client/clientset/versioned"
@@ -48,176 +42,6 @@ type PodMetricsList struct {
 	} `json:"items"`
 }
 
-// func getMetrics(clientset *kubernetes.Clientset, pods *PodMetricsList) error {
-// 	data, err := clientset.RESTClient().Get().AbsPath("apis/metrics.k8s.io/v1beta1/pods").DoRaw(context.TODO())
-// 	if err != nil {
-// 		return err
-// 	}
-// 	err = json.Unmarshal(data, &pods)
-// 	return err
-// }
-
-/*
-func main() {
-	namespace := os.Args[1]
-	fmt.Println(namespace)
-	// var kubeconfig, master string
-	// config, err := clientcmd.BuildConfigFromFlags(master, kubeconfig)
-    // if err != nil{
-    //     panic(err)
-    // }
-
-    // mc, err := metrics.NewForConfig(config)
-    // if err != nil {
-    //     panic(err)
-    // }
-	// fmt.Println(mc.MetricsV1beta1().PodMetricses(metav1.NamespaceAll).List(context.Background(),metav1.ListOptions{}))
-	rules := clientcmd.NewDefaultClientConfigLoadingRules()
-	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, &clientcmd.ConfigOverrides{})
-	config, err := kubeconfig.ClientConfig()
-	if err != nil {
-		log.Printf("Error in new client config: %s\n",err)
-	}
-	clientset := kubernetes.NewForConfigOrDie(config)
-	// *********** client set for metrics *********
-	clientsetMetrics, err := metrics.NewForConfig(config)
-	if err != nil {
-		log.Println("Error in new config: \n",err)
-	}
-	// ************** node metrics *************
-	nodeList, err := clientset.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		log.Printf("Error in getting pods list for namespace %s: %s\n",namespace, err)
-	}
-	for i, n := range nodeList.Items {
-		fmt.Printf("Pod %d in %s namespace: %s \n",i, namespace,n.Name)
-	}
-	nodeMetricsList, err := clientsetMetrics.MetricsV1beta1().NodeMetricses().List(context.Background(), metav1.ListOptions{})
-
-	if err != nil {
-		log.Printf("Error in getting pod metrics for namespace %s: %s\n",namespace, err)
-	}
-	for _, v := range nodeMetricsList.Items {
-		fmt.Printf("%s\n", v.GetName())
-		fmt.Printf("%s\n", v.GetNamespace())
-		fmt.Printf("%vm\n", v.Usage.Cpu().MilliValue())
-		fmt.Printf("%vMi\n", v.Usage.Memory().Value()/(1024*1024))
-	}
-	// ************* pod metrics ************
-	podList, err := clientset.CoreV1().Pods(namespace).List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		log.Printf("Error in getting pods list for namespace %s: %s\n",namespace, err)
-	}
-	for i, n := range podList.Items {
-		fmt.Printf("Pod %d in %s namespace: %s \n",i, namespace,n.Name)
-	}
-
-	podMetricsList, err := clientsetMetrics.MetricsV1beta1().PodMetricses(namespace).List(context.Background(), metav1.ListOptions{})
-
-	if err != nil {
-		log.Printf("Error in getting pod metrics for namespace %s: %s\n",namespace, err)
-	}
-	for _, v := range podMetricsList.Items {
-		fmt.Printf("%s\n", v.GetName())
-		fmt.Printf("%s\n", v.GetNamespace())
-		fmt.Printf("%dm\n", v.Containers[0].Usage.Cpu().MilliValue())
-		fmt.Printf("%dMi\n", v.Containers[0].Usage.Memory().Value()/(1024*1024))
-	}
-
-	// ********** new pod creation **********
-	// newPod := &corev1.Pod{
-	// 	ObjectMeta: metav1.ObjectMeta{
-	// 	  Name: "test-pod",
-	// 	},
-	// 	Spec: corev1.PodSpec{
-	// 	  Containers: []corev1.Container{
-	// 		{Name: "busybox", Image: "busybox:latest",Command: []string{"sleep", "100000"}},
-	// 	  },
-	// 	},
-	//   }
-
-	// pod, err := clientset.CoreV1().Pods(namespace).Create(context.Background(), newPod, metav1.CreateOptions{})
-	// if err != nil {
-	// 	log.Printf("Error in creating pod for namespace %s: %s",namespace, err)
-	// }
-	// fmt.Println(pod)
-
-
-	// var pods PodMetricsList
-	// err = getMetrics(clientset, &pods)
-	// if err != nil {
-	// 	log.Println(err.Error())
-	// }
-	// for _, m := range pods.Items {
-	// 	fmt.Println(m.Metadata.Name, m.Metadata.Namespace, m.Timestamp.String())
-	// }
-	// mc, err := metrics.NewForConfig(configMetrics)
-	// if err != nil {
-	// 	log.Println(err)
-	// }
-
-	// podMetrics, err := mc.MetricsV1beta1().PodMetricses(namespace).List(metav1.ListOptions{})
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
-	// for _, podMetric := range podMetrics.Items {
-	// 	podContainers := podMetric.Containers
-	// 	for _, container := range podContainers {
-	// 		cpuQuantity, ok := container.Usage.Cpu().AsInt64()
-	// 		memQuantity, ok := container.Usage.Memory().AsInt64()
-	// 		if !ok {
-	// 			return
-	// 		}
-	// 		msg := fmt.Sprintf("Container Name: %s \n CPU usage: %d \n Memory usage: %d", container.Name, cpuQuantity, memQuantity)
-	// 		fmt.Println(msg)
-	// 	}
-
-	// }
-
-}
-
-*/
-/*
-func main(){
-	//namespace := "cainaticsmongo"
-    // nodeName := "aks-memorypool-30666732-vmss000002"
-	rules := clientcmd.NewDefaultClientConfigLoadingRules()
-	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, &clientcmd.ConfigOverrides{})
-	config, err := kubeconfig.ClientConfig()
-	if err != nil {
-		log.Printf("Error in new client config: %s\n",err)
-	}
-	clientset := kubernetes.NewForConfigOrDie(config)
-    // events, err := clientset.CoreV1().Events(namespace).List(context.Background(), metav1.ListOptions{
-    //     TypeMeta: metav1.TypeMeta{
-    //         Kind: "Node",
-    //     },
-    //     FieldSelector: "involvedObject.name=" + nodeName,
-    // })
-	// fmt.Println(events)
-	podList, err := clientset.CoreV1().Pods("jenkins").List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		log.Printf("Error in getting pods list for namespace %s: %s\n","jenkins", err)
-	}
-	fmt.Println(len(podList.Items))
-	// for _, n := range podList.Items {
-	// 	fmt.Println(n.Spec.NodeName)
-	// }
-	// nodes, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
-
-    // if err != nil {
-    //     fmt.Println(err)
-    // }
-	// for _, v := range nodes.Items{
-	// 	if v.Name == "aks-memorypool-30666732-vmss000001"{
-
-	// 		fmt.Println(v.Status.Capacity.Cpu())
-	// 	}
-	// }
-
-}
-*/
 type podGraph struct {
 	nodeName string
 	containerName string
@@ -280,11 +104,6 @@ func GetNodeCapacity(nodeName string) (int64, *resource.Quantity) {
 	for _, v := range nodes.Items {
 		
 		if v.Name == nodeName {
-			// fmt.Println(v.Name)
-			// fmt.Println("Node CPU Value: ", v.Status.Capacity.Cpu())
-			// fmt.Println("Node CPU MiliValue: ", v.Status.Capacity.Cpu().MilliValue())
-			// fmt.Println("Node Memory: ", v.Status.Capacity.Memory())
-			// fmt.Println("Node Memory Value: ", v.Status.Capacity.Memory().Value())
 			return v.Status.Capacity.Cpu().MilliValue(), v.Status.Capacity.Memory()
 		}
 	}
@@ -312,7 +131,6 @@ func GetNodeMetrics() ([][]string){
 	if err != nil {
 		log.Printf("Error in getting nodes list : %s\n", err)
 	}
-	// nodeBox := MyNode{}
 	for _, n := range nodeMetricsList.Items {
 		nodeName := n.Name
 		
@@ -322,13 +140,10 @@ func GetNodeMetrics() ([][]string){
 		memoryFloat := float64(memory.Value()/(1024 * 1024))
 		nodeCpuUsage := float64(n.Usage.Cpu().MilliValue()) * 1000
 		nodeMemoryUsage := float64(n.Usage.Memory().Value())/(1024.0 * 1024)
-		// numberPods := n.Usage.Pods().Size()
 		pods, _:= clientset.CoreV1().Pods("").List(context.Background(),metav1.ListOptions{
 			FieldSelector: "spec.nodeName=" + nodeName,
 		})
 		numberPods := len(pods.Items)
-		// item := nodeGraph{nodeName: nodeName, nodeCpuPercentage: (nodeCpuUsage/(cpuFloat)) * 100, nodeMemory: memoryFloat,nodeMemoryPercentage: nodeMemoryUsage/(memoryFloat) * 100, noOfPods: numberPods}
-		// nodeBox.Graph = append(nodeBox.Graph, item)
 		data = append(data, []string{nodeName, fmt.Sprintf("%v", cpu),fmt.Sprintf("%v",n.Usage.Cpu().MilliValue()) ,fmt.Sprintf("%v",(nodeCpuUsage/(cpuFloat)) * 100), fmt.Sprintf("%v", memory),fmt.Sprintf("%v",memoryValue),fmt.Sprintf("%v", nodeMemoryUsage/(memoryFloat) * 100), fmt.Sprintf("%v",numberPods)})
 
 	}
@@ -351,12 +166,9 @@ func GetPodMetrics(namespace string) ([][]string, MyBox){
 		for _, container := range n.Containers{	
 			containerCpuFloat:= float64(container.Usage.Cpu().MilliValue())
 			containerMemoryFloat := float64(container.Usage.Memory().Value())/(1024.0 * 1024.0)
-			// item := podGraph{nodeName: nodeName, containerName: container.Name, podName: n.Name, containerCpu: containerCpuFloat, cpuPercentage: (containerCpuFloat/(cpuFloat)) * 100, containerMemory: containerMemoryFloat, memoryPercentage: containerMemoryFloat/(memoryFloat) * 100}
-			// box.Graph = append(box.Graph, item)
 			data = append(data, []string{nodeName, n.Name, container.Name, fmt.Sprintf("%v",containerCpuFloat), fmt.Sprintf("%v", (containerCpuFloat/cpuFloat) * 100), fmt.Sprintf("%v", containerMemoryFloat), fmt.Sprintf("%v",(containerMemoryFloat/memoryFloat) * 100)})
 		}
 	}
-	//fmt.Println(box)
 	return data, box
 
 }
--- guigo.go
+++ guigo.go
@@ -5,96 +5,10 @@ import (
 	"fmt"
 	"os"
 	"time"
-
-	//"image/color"
 	graph "kubernetes-go/graph"
 	"math"
-	//"os/exec"
-
-	// "fyne.io/fyne/v2"
-	// "fyne.io/fyne/v2/app"
-	// "fyne.io/fyne/v2/canvas"
-	// "fyne.io/fyne/v2/container"
-	// "fyne.io/fyne/v2/layout"
-	// "fyne.io/fyne/v2/widget"
 	"github.com/aquasecurity/table"
 )
-
-// func makeTable(data [][]string) *widget.Table{
-// 	newlist := widget.NewTable(
-// 		func() (int, int) {
-// 			return len(data), len(data[0])
-// 		},
-// 		func() fyne.CanvasObject {
-// 			return widget.NewLabel("wide content")
-// 		},
-// 		func(i widget.TableCellID, o fyne.CanvasObject) {
-// 			o.(*widget.Label).SetText(data[i.Row][i.Col])
-// 		})
-// 	return newlist
-// }
-/*
-func main(){
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Kubernetes metrics")
-	myWindow.Resize(fyne.NewSize(850, 950))
-	title := canvas.NewText("Get graphs for pods and nodes", color.White)
-	title.TextStyle = fyne.TextStyle{
-		Bold: true,
-	}
-	title.Alignment = fyne.TextAlignCenter
-	title.TextSize = 24
-	namespace := widget.NewEntry()
-	namespace.SetPlaceHolder("Enter namespace...")
-	entity := widget.NewEntry()
-	entity.SetPlaceHolder("Enter entity name... either pods or nodes")
-
-	// if fmt.Sprintf("%v",entity) == fmt.Sprintf("%v", "pods"){
-	// 	graph.GenerateGraphPods(fmt.Sprintf("%v", namespace))
-	// }
-	logo := canvas.NewImageFromFile("1200px-Fyne_toolkit_logo.svg.png")
-	factText := widget.NewLabel("")
-	factText.Wrapping = fyne.TextWrapWord
-	btn := widget.NewButton("Show graph", nil)
-	changed := func(){
-		fmt.Println(namespace.Text)
-		fmt.Println(entity.Text)
-		if entity.Text == "pods"{
-			data := graph.GenerateGraphPods(namespace.Text)
-			//list  = makeTable(data)
-			words := ""
-			words = words + "Pod Name" +"\t\t\t\t\t" +"Container Name" + "\t\t\t\t\t"+"CPU Percentage" +"\t\t\t\t\t"+ "Memory Percentage"+"\n\n"
-			for _, c := range data{
-				words = words + c[0]+"\t\t"+c[1]+"\t\t"+c[2]+"\t\t"+c[3] + "\n\n"
-
-			}
-			factText.SetText(words)
-			// img := canvas.NewImageFromFile("cpu_consumerclf.png")
-			// img1 := canvas.NewImageFromFile("memory_consumerclf.png")
-			// imagesGraph = container.NewVSplit(img, img1)
-
-		}
-	}
-	btn.OnTapped = changed
-
-
-
-	vBox := container.New(layout.NewVBoxLayout(), title, namespace,entity, btn, factText)
-	myWindow.SetContent(container.NewHSplit(vBox, logo))
-	myWindow.ShowAndRun()
-	cmd := exec.Command("feh", "output1.png")
-    stdout, err := cmd.Output()
-
-    if err != nil {
-        fmt.Println(err.Error())
-        return
-    }
-
-    // Print the output
-    fmt.Println(string(stdout))
-
-}
-*/
 
 func main() {
 	
@@ -103,9 +17,6 @@ func main() {
 	help := flag.String("h", "","shows Usage of the command line arguments")
 
 	flag.Parse()
-	//if help == ""
-	// namespace := os.Args[1]
-	// entity := os.Args[2]
 	if *help == ""{
 		flag.PrintDefaults()
 	}
